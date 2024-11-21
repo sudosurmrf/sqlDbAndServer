d320de3d27e241ceb828a2429be13378118da7e2
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-11-20 21:17:41 -0800

    added correct require dot env and package json

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_store_db');
 const uuid = require('uuid');
